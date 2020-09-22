d8225f7d2c59dc33fe8840714bed96876f7bcc6a
saka-shun <sakashun714@gmail.com>
2020-09-22 17:40:15 +0900

    新規登録、ログイン、ログアウトの実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :password, :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday_date])
+  end
 
   private
 
@@ -8,4 +15,5 @@ class ApplicationController < ActionController::Base
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
   end
+
 end
