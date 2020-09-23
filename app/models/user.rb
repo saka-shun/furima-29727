a86db21466b4ac41f1e8190e88bae5c61ce99871
saka-shun <sakashun714@gmail.com>
2020-09-23 12:22:32 +0900

    バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :nickname,        presence: true
+  validates :email,           presence: true
+  validates :password,        presence: true, length: { minimum: 6 }
+  validates :last_name,       presence: true
+  validates :first_name,      presence: true
+  validates :last_name_kana,  presence: true
+  validates :first_name_kana, presence: true
+  validates :birthday_date,   presence: true
+
 end
