7b106aa4f6454f1880c88b72245ce49aae32c092
saka-shun <sakashun714@gmail.com>
2020-09-25 18:14:48 +0900

    gitignoreの編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  validates :nickname,        presence: true
-  validates :email,           presence: true
-  validates :password,        presence: true, length: { minimum: 6 }
-  validates :last_name,       presence: true
-  validates :first_name,      presence: true
-  validates :last_name_kana,  presence: true
-  validates :first_name_kana, presence: true
-  validates :birthday_date,   presence: true
-
+  with_options presense: true do
+     validates :nickname,        
+     validates :email,           
+     validates :password,        length: { minimum: 6 }
+     validates :last_name,       
+     validates :first_name,      
+     validates :last_name_kana,  
+     validates :first_name_kana, 
+     validates :birthday_date,   
+  end
 end
